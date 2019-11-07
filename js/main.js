ad87d8062a4257ce5da51a4afcaf7146a1eed7a6
CGVA3 <cdgva@outlook.com>
2019-11-06 22:53:03 -0500

    hi

--- js/main.js
+++ js/main.js
@@ -18,6 +18,26 @@ $("#b_sp").on("click", () => {
  window.location.href = "questions.html";
 
 })
+$("#sg_1").on("click", () => {
+
+ window.location.href = "questions2.html";
+
+})
+$("#sg_2").on("click", () => {
+
+ window.location.href = "questions3.html";
+
+})
+$("#sg_3").on("click", () => {
+
+ window.location.href = "questions4.html";
+
+})
+$("#sg_4").on("click", () => {
+
+ window.location.href = "result.html";
+
+})
 
 const loadContent = () => {
  
